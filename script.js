2fafe2318bb49ef4dc2a8c421db187e19a1d51d4
bhkstore0-source <bhk.store0@gmail.com>
2026-04-28 19:15:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 const PRODUCT_PRICE      = 4800;
 const PRODUCT_NAME       = 'آلة الحلاقة kemei للنساء 2EN1';
-const SCRIPT_URL         = 'https://script.google.com/macros/s/AKfycbw9gJkYsv_zBEARaLICS89W-WCWcF0Fgnytoy2x0bS8hglW2Ypo8riuvCS5NUD6Tz_m/exec';
+const SCRIPT_URL         = 'https://script.google.com/macros/s/AKfycbxANz4iuRsmU3XwKsJhmQ1caPlFpBnLqMOrgRlIdtjjtQpuPOcuXq45sy5uEZbNCiq-WA/exec';
 const WHATSAPP_NUM       = '213553096569';
 const RESTRICTED_WILAYAS = ['52', '56', '57'];
 
